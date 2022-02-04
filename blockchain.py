0559d9cbc2a72d2c01eb4add253aee7c580f436c
Divyajyoti <divyajyoti182000@gmail.com>
2022-02-04 18:34:53 +0530

    Created proof_of_work + hash function

--- blockchain.py
+++ blockchain.py
@@ -24,14 +24,30 @@ class Blockchain:
         return self.chain[-1]
         
     #proof of work is a specific number founded by a miner hard to find and 
-    #easy to verify
+    #easy to verify it is improtant to mine a new block
     
     def proof_of_work(self, previous_proof):
         new_proof=1 #we have to increment this and check wheather it correct
-        check_proof=False
+        # we are solving problem with hit & trial method 
+        check_proof=False #intaillaly remains False
         while check_proof is False:
             #problem miner have to solve
-            hash_operation=hashlib
-        
-        
+            #number of zero increases the more tougher it will be to solve the problem
+            #proof needed to be asymetrical operation in nature
+            hash_operation=hashlib.sha256(str(new_proof**2-previous_proof**2).encode()).hexdigest()
+            #using haslib.sha256 function defining asymetric problem str formate and
+            #encoding fucntion to encode function
+            #since hashes in blockchain is in hexadecimal so hexdigest()
+            if hash_operation[:4]=='0000': #the upper-bound is always excluded
+                check_proof=True
+            else:
+                new_proof += 1;
+        return new_proof
+    
+    def hash(silf,block):
+        #This function will give us dedicatred hash of each block an since our 
+        #contains data into dictinoary format but will change it to json format and use
+        #Json damp function
+        encoded_block = json.dumps(block,sort_keys=True).encode() #converted to json
+        return hashlib.sha256(encoded_block).hexdigest() #hexadecimal format
 
